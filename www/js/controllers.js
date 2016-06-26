52e9a7f12bd0508c40f98ebfb3a37d1fa443e86c
Jongseong Lee <dazzyjong@gmail.com>
2016-06-26 17:05:06 +0900

    add activity function

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -20,36 +20,37 @@ angular.module('hyocare.controllers', ['hyocare.user', 'hyocare.activities'])
 
 })
 
-.controller('CareActivitiesCtrl', function($scope, $state, $ionicPopup, Activities) {
+.controller('CareActivitiesCtrl', function($scope, $state, $ionicPopup, $window, Activities) {
   $scope.carerecipientId = $state.params.carerecipientId;
+  var Break = new Error('Break');
 
     $scope.feelings = [
-        { level: "절망", checked: false },
-        { level: "슬픔", checked: false },
-        { level: "우울", checked: false },
-        { level: "행복", checked: false }
+        { level:"1", label: "절망", checked: false },
+        { level:"2", label: "슬픔", checked: false },
+        { level:"3", label: "우울", checked: false },
+        { level:"4", label: "행복", checked: false }
     ];
 
     $scope.status_physical = [
-      { level: "많이 나빠잠",  checked: false},
-      { level: "조금 나빠잠",  checked: false},
-      { level: "보통",       checked: false},
-      { level: "조금 좋아짐",  checked: false},
-      { level: "매우 좋음",   checked: false}
+      { level:"1", label: "많이 나빠잠",  checked: false},
+      { level:"2", label: "조금 나빠잠",  checked: false},
+      { level:"3", label: "보통",       checked: false},
+      { level:"4", label: "조금 좋아짐",  checked: false},
+      { level:"5", label: "매우 좋음",   checked: false}
     ];
     $scope.status_meal = [
-      { level: "많이 나빠잠",  checked: false},
-      { level: "조금 나빠잠",  checked: false},
-      { level: "보통",       checked: false},
-      { level: "조금 좋아짐",  checked: false},
-      { level: "매우 좋음",   checked: false}
+      { level:"1", label: "많이 나빠잠",  checked: false},
+      { level:"2", label: "조금 나빠잠",  checked: false},
+      { level:"3", label: "보통",       checked: false},
+      { level:"4", label: "조금 좋아짐",  checked: false},
+      { level:"5", label: "매우 좋음",   checked: false}
     ];
     $scope.status_cognitive = [
-      { level: "많이 나빠잠",  checked: false},
-      { level: "조금 나빠잠",  checked: false},
-      { level: "보통",       checked: false},
-      { level: "조금 좋아짐",  checked: false},
-      { level: "매우 좋음",   checked: false}
+      { level:"1", label: "많이 나빠잠",  checked: false},
+      { level:"2", label: "조금 나빠잠",  checked: false},
+      { level:"3", label: "보통",       checked: false},
+      { level:"4", label: "조금 좋아짐",  checked: false},
+      { level:"5", label: "매우 좋음",   checked: false}
     ];
 
     $scope.status_defecation = [
@@ -66,48 +67,79 @@ angular.module('hyocare.controllers', ['hyocare.user', 'hyocare.activities'])
       { level: "0", checked: false}
     ];
 
-  $scope.status_comment="동일";
-
   $scope.activities = {
     carerecipient_code: $scope.carerecipientId,
     status_defecation:'',
     status_physical:'',
     status_meal:'',
     status_cognitive:'',
-    status_comment:''
+    status_comment:'동일'
   };
-    $scope.updateSelection = function(position, feelings, step) {
-        angular.forEach(feelings, function(subscription, index) {
-          console.log("position " + position + " index " + index);
+    $scope.updateSelection = function(position, objs, level) {
+
+      switch(objs) {
+        case $scope.feelings:
+          console.log("feeling");
+        break;
+        
+        case $scope.status_physical:
+        	$scope.activities.status_physical = level.valueOf();
+        break;
+
+        case $scope.status_meal:
+          $scope.activities.status_meal = level.valueOf();
+        break;
+
+        case $scope.status_cognitive:
+        	$scope.activities.status_cognitive = level.valueOf();
+        break;
+        
+        case $scope.status_defecation:
+        	$scope.activities.status_defecation = level.valueOf();
+        break;
+
+        case $scope.status_big_defecation:
+        console.log("status_big_defecation");
+        break;
+      }
+        angular.forEach(objs, function(obj, index) {
             if (position != index)
-                subscription.checked = false;
-                $scope.selected = step;
+                obj.checked = false;
+                //$scope.selected = level;
             }
         );
+        console.log($scope.activities);
     }
 
     $scope.changeBack = function(obj) {
-
-      $scope.status_comment = obj.status_comment;
-      console.log($scope.status_comment);
+      $scope.activities.status_comment = obj.activities.status_comment.valueOf();
     }
 
   $scope.report = function() {
-    if($scope.activities) {
-      var alertPopup = $ionicPopup.alert({
-      title: 'Don\'t eat that!',
-      template: 'It might taste good'
+    try{
+      angular.forEach($scope.activities, function(value, key) {
+        if(value == '') {
+          var alertPopup = $ionicPopup.alert({
+            title: '모두 입력 해주세요',
+          });
+          
+          alertPopup.then(function(res) {
+          });
+
+          throw Break;
+        }
       });
-      
-      alertPopup.then(function(res) {
-        // nothing
-      });
-    } else {
-      Activities.report($window.sessionStorage.getItem('code'), $scope.activities)
+    } catch(e) {
+      if(e != Break) {
+        throw Break;
+      }
+      return;
+    }
+
+    Activities.report($window.sessionStorage.getItem('code'), $scope.activities)
       .then(function(){
         $state.go('tab.care');
       });
-    };
   };
 })
 
